eb761323ce29e7d0ba7039273405c5b1de60599c
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-11-09 16:13:24 +0100

    First update translation file

--- footer.php
+++ footer.php
@@ -18,11 +18,12 @@
                 <?php dynamic_sidebar('home-left'); ?>
             <?php else: ?>
                 <div class="header">
-                    <h1 class="header__title">O blogu</h1>
+
+                    <h1 class="header__title"><?php _e( 'About blog', 'mc_theme' ); ?></h1>
                     <span class="header__strip"></span>
                 </div>
-                <p>To jest miejsce do mniej formalnej komunikacji, wymiany doświadczeń oraz udostępnienia tych rzeczy,
-                    które dla Państwa mogą być istotne i ciekawe.</p>
+                <p><?php _e( 'This is the place for less formal communication, sharing experiences and making things available that can be important and interesting to you.', 'mc_theme' ); ?></p>
+
             <?php endif; ?>
 
         </div>
@@ -31,10 +32,12 @@
                 <?php dynamic_sidebar('home-right'); ?>
             <?php else: ?>
                 <div class="header">
-                    <h1 class="header__title">Znajdź nas</h1>
+                    <h1 class="header__title">
+                        <?php _e( 'Find us', 'mc_theme' ); ?>
+                    </h1>
                 </div>
-                <p>Adres<br> Królewska 27<br> 00-500 Warszawa<br></p>
-                <p>Godziny<br>Poniedziałek—piątek: 8:15–16:15</p>
+                <p><?php _e( 'Address<br>Królewska 27<br> 00-500 Warsaw', 'mc_theme' ); ?></p>
+                <p><?php _e( 'Openning hours<br>Monday-Friday 8:15-16:15', 'mc_theme' ); ?></p>
             <?php endif; ?>
         </div>
     </div>
@@ -43,15 +46,13 @@
         <div class="col-6">
             <a href="http://mc.bip.gov.pl/">
                 <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIIAAACYBAMAAADaYCwzAAAAMFBMVEXRFjLqk6DcS2H54eTzwMjkeIj////WLUftoq387/HhZ3r1zNLaQVjvsLrmg5LfW26G8voPAAAACXBIWXMAAAsTAAALEwEAmpwYAAACUElEQVRo3u2Yvy8EQRTH313I+nHhKolotDqFXIO45JAglGrijDvnklVQqy4SjShWROEqhUriR6JQaCQ6hYhG4i+Qq9Rm3uzc7kUkM/vQeN+Cm5X77Pe9eW9mDADrh/ROFQiqmMCE/0gYz8UG84E7oQSpaDAGbe6EfYDIRB08d8I5wHVz4MdxiQiDAAGNkIceYhSVs2ciIVE9KEL58kmHX34xiay8FhwI8w2ATL8aHJp6mJWzspWzJvhqV/BqcvAGGXw4gRtFjzVBq0sOpBkMwdePHtwIsNgkmGeZwI2QMoSybx6d2hLSeyN1UAWtCavy8+bFgvzZZkuo6dAXQ4JstnZsVMyrDSEV/t4OCVltXwVzbUe4F9p6d0hohP31CNBnR+jDGQToCAk+9OKfltCLDUF3k4pGE0wtbQB8/Alh+GsU4BbF2tdMQrjWWWZyPT6bdfwuRLOZsyAUAd9UxIp602+VbRZgRaWtdhy5umYGbnVV57GQVUdszk3ZVrVKpVYHJgWmRXSUtOosDMN0dxVUNpqAtFV3Y8qVVDOpyeiMCEOWe3dlEFc5rKsD7cEzC4bl7l+Uaeyt6cW67qk8pK7k1NwFDueH0YLZ7FZmdH2vTN4QTiAQPxD8Y0JDdimNcOI985mWCb9H2KWKry5YLBaLxWKxWCwWi8X6VkfuOm4lJPl3/5xMqJAJrSYE2USyi488mVAkE+ImBNmEIJsQZBOCbCIxoUommPt8AqFKJhgTgmyCci24QyaUyARtQpBN0C5Is2TCMpmgTAiyCUE2QSUsfwLRnGLgIT/pkgAAAABJRU5ErkJggg==">
-                Przejdź do strony Biuletynu Informacji Publicznej Ministerstwa Cyfryzacji
+                <?php _e( 'Go to the Public Information Bulletin of the Ministry of Digital Affairs', 'mc_theme' ); ?>
             </a>
         </div>
         <div class="col-6">
-            <p>Artykuły zamieszczone w serwisie GOV.PL, w których nie podajemy żadnych dodatkowych informacji na temat
-                praw autorskich, należą do informacji publicznych i udostępniamy je bezpłatnie. Korzystanie z nich,
-                niezależnie od celu i sposobu korzystania, nie wymaga zgody Ministerstwa. Dostępne są w ramach licencji
-                <a href="https://creativecommons.org/licenses/by/3.0/pl/">Creative Commons Uznanie Autorstwa 3.0
-                    Polska</a>.</p>
+            <p>
+                <?php printf(__('The articles on GOV.PL, which do not provide any additional information on copyright, are public information and are provided free of charge. Using them, regardless of purpose and usage, does not require the approval of the Ministry. Available under license <a href="%s">Creative Commons Attribution 3.0 Poland</a>', 'mc_theme' ), "https://creativecommons.org/licenses/by/3.0/pl/"); ?>
+            </p>
         </div>
     </div>
     <footer class="authorship site-footer">
