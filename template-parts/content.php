29f1bd110ad1c3b53cf62400172212142aacec74
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-11-11 18:02:33 +0100

    Fix positioning .article__metrics - close #3

--- template-parts/content.php
+++ template-parts/content.php
@@ -8,60 +8,63 @@
  */
 
 ?>
-    <article id="post-<?php the_ID(); ?>" <?php post_class("row"); ?>>
+    <article id="post-<?php the_ID(); ?>" class="article">
         <?php
         if (is_singular() && has_post_thumbnail()) :
             the_post_thumbnail('large', ['class' => 'article__cover']);
         endif;
         ?>
-        <div class="col-9">
-            <header class="article__header">
-                <?php
-                if (is_singular()) :
-                    the_title('<h1 class="article__title">', '</h1>');
-                    ?><div class="header__strip"></div><?php
-                else :
-                    the_title('<h2 class="article__title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-                endif;
-                ?>
-            </header><!-- .article__header -->
-
-            <div class="article__content">
-                <?php
-                the_content(sprintf(
-                    wp_kses(
-                    /* translators: %s: Name of current post. Only visible to screen readers */
-                        __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'mc_theme'),
-                        array(
-                            'span' => array(
-                                'class' => array(),
-                            ),
-                        )
-                    ),
-                    get_the_title()
-                ), !is_singular());
-
-                ?>
-            </div><!-- .article__content -->
-            <?php if (is_singular()) : ?>
-                <div class="article__navigation">
+        <div class="row">
+            <div class="col-9">
+                <header class="article__header">
                     <?php
-                    echo mc_theme_the_post_navigation();
+                    if (is_singular()) :
+                        the_title('<h1 class="article__title">', '</h1>');
+                        ?>
+                        <div class="header__strip"></div><?php
+                    else :
+                        the_title('<h2 class="article__title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+                    endif;
                     ?>
-                </div><!-- .article__navigation -->
-            <?php endif; ?>
-        </div>
-        <div class="col-3">
-            <div class="article__metrics">
-                <?php mc_theme_metric(); ?>
+                </header><!-- .article__header -->
+
+                <div class="article__content">
+                    <?php
+                    the_content(sprintf(
+                        wp_kses(
+                        /* translators: %s: Name of current post. Only visible to screen readers */
+                            __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'mc_theme'),
+                            array(
+                                'span' => array(
+                                    'class' => array(),
+                                ),
+                            )
+                        ),
+                        get_the_title()
+                    ), !is_singular());
+
+                    ?>
+                </div><!-- .article__content -->
+                <?php if (is_singular()) : ?>
+                    <div class="article__navigation">
+                        <?php
+                        echo mc_theme_the_post_navigation();
+                        ?>
+                    </div><!-- .article__navigation -->
+                <?php endif; ?>
             </div>
-            <?php if (has_post_thumbnail() and !is_singular()): ?>
-                <div class="article__icon">
-                    <?php
-                    the_post_thumbnail("thumbnail", ['class' => "post-index__thumbnail"]);
-                    ?>
+            <div class="col-3">
+                <div class="article__metrics">
+                    <?php mc_theme_metric(); ?>
                 </div>
-            <?php endif; ?>
+                <?php if (has_post_thumbnail() and !is_singular()): ?>
+                    <div class="article__icon">
+                        <?php
+                        the_post_thumbnail("thumbnail", ['class' => "post-index__thumbnail"]);
+                        ?>
+                    </div>
+                <?php endif; ?>
+            </div>
         </div>
     </article><!-- #post-<?php the_ID(); ?> -->
 <?php if (comments_open() || get_comments_number()) : ?>
