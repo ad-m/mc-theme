c26ac38f997c90c351d5cae036ae04ad2032cb0d
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-11-22 17:40:56 +0100

    Drop builded files

--- static/js/topbar.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    Array.prototype.slice.call(document.querySelectorAll('.menu-handler')).forEach(function(item) {
-        item.addEventListener('click', function(ev) {
-            ev.preventDefault();
-            var menu = document.querySelector('.navbar__menu-list');
-            menu.setAttribute('aria-expanded', menu.getAttribute('aria-expanded') === 'true' ? 'false' : 'true' );
-        })
-    })
-})();
